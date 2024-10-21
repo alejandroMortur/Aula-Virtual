3541564d078b7a21f79334da072e759eec2241ac
AlexDEV <moronturielalejandro@gmail.com>
2024-10-21 19:24:20 +0200

    CSS percentage complete message for cards

--- public/view/commonViews/courses/courses.js
+++ public/view/commonViews/courses/courses.js
@@ -10,12 +10,14 @@ function printCourses() {
     let coursesHTML = "";
 
     coursesList.forEach(element => {
+
+        let workDoneMensage = element.porcent+" "+element.workDone;
         
         coursesHTML += `
                         <div class="card" id="${element["id"]}" onclick="clickCard(event)">
                             <img src='${element["image"]}' id="header">
                             <h1 id="card__title">${element["title"]}</h1>
-                            <p id="card__test">${element["description"]}</p>
+                            <p id="card__test">${workDoneMensage}</p>
                         </div>`;
 
     });

bf86308521bf9add7e35e8389dcc0cab61057c98
AlexDEV <moronturielalejandro@gmail.com>
2024-10-25 20:35:49 +0200

    menu js with dom structure and css grid name set

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,28 +1,44 @@
 function loadMenu(currentUser){
 
+    let menuSrc = document.getElementById('menu');
     let Fullname = currentUser.Name+" "+currentUser.SureName;
 
-    let HTMLMenu = `
-                    <header>
-                    <div id="Left">
-                        <img src="/public/assets/icons/logo.jpg">
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="/public/view/commonViews/courses/courses.html" class="boton">Cursos</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div id="Right">
-                        <img id="profilePicture" src="${currentUser.Image}"><p>${Fullname}</p>
-                    </div>
-                    </header>
-                    `;
+    let header = document.createElement('header');
+    menuSrc.appendChild(header);
 
-        document.getElementById('menu').innerHTML += HTMLMenu;
+    let divLeft = document.createElement('div');
+    divLeft.id = "Left";
+    header.appendChild(divLeft);
 
+    let img = document.createElement('img');
+    img.src ="/public/assets/icons/logo.jpg";
+    divLeft.appendChild(img);
 
+    let nav = document.createElement('nav');
+    divLeft.appendChild(nav);
 
+    let ul = document.createElement(`ul`);
+    nav.appendChild(ul);
+
+    let li = document.createElement(`li`);
+    ul.appendChild(li);
+
+    let coursesSrc = document.createElement('a');
+    coursesSrc.href ="/public/view/commonViews/courses/courses.html"
+    coursesSrc.className ="boton"
+    coursesSrc.innerHTML = "Cursos"
+    li.appendChild(coursesSrc)
+
+    let divRight = document.createElement('div');
+    divRight.id = "Right";
+    header.appendChild(divRight);
+
+    let UserImg = document.createElement('img');
+    UserImg.src = currentUser.Image;
+    divRight.appendChild(UserImg);
+
+    let UserName = document.createElement('p');
+    UserName.innerHTML = Fullname;
+    divRight.appendChild(UserName);
 
 }

6727a7c883f78afd984ea038ad5242cc27f31afc
AlexDEV <moronturielalejandro@gmail.com>
2024-10-27 14:16:29 +0100

    coursesTask  css temporal base upload and dom  js code upload por each task card

---
+++ public/view/commonViews/coursesTasks/coursesTask.js
@@ -0,0 +1,49 @@
+let userCoursesTaskList = []
+
+function printTask(currentCourse){
+
+    let taskContainer = document.getElementById('taskContainer');
+    let tasks = currentCourse.getTask();
+
+    tasks.forEach(element => {
+        
+        let taskCard = document.createElement('div');
+        taskCard.className = "taskCard";
+        taskCard.id = element["task_id"];
+        taskCard.addEventListener("click",clickCard)
+        taskContainer.appendChild(taskCard);
+
+        let taskImage = document.createElement('img')
+        taskImage.src = "/public/assets/icons/media-icons/file-72px.png";
+        taskCard.appendChild(taskImage);
+
+        let taskName = document.createElement('p')
+        taskName.innerHTML = element.getName();
+        taskCard.appendChild(taskImage);
+
+        let taskDescription = document.createElement('p')
+        taskDescription.innerHTML = element.getDescription();
+        taskCard.appendChild(taskDescription);
+
+    });
+
+    userCoursesTaskList = tasks;
+
+}
+
+function clickCard(event){
+
+    const id = event.currentTarget.id;
+
+    userCoursesTaskList.forEach(element => {
+        
+        if(element["task_id"] == id){
+
+            sessionStorage.setItem("task", JSON.stringify(element));
+            window.location.href = "/public/view/commonViews/tasks/task.php";
+
+        }
+
+    });
+
+}

a0284edcdd48f7d3c906c15da5b5ed89c1277d0b
AlexDEV <moronturielalejandro@gmail.com>
2024-10-28 22:41:40 +0100

    main server php add to save post files from html page

--- server/main.php
+++ server/main.php
@@ -4,11 +4,56 @@
 
     if(isset($_POST['Enviar'])){
 
-        $files = $_FILES['Ficheros'];
-        echo "<script>console.log('Archivos recibidos correctamente');</script>";
+        if (isset($_POST['type'])) {
 
-    
-    /*} else if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Limpieza y validación de entrada para evitar inyección de código
+            $tipoSubida = htmlspecialchars(trim(strip_tags($_POST['type'])), ENT_QUOTES, 'UTF-8');
+
+            // Añadir un punto y coma después de la definición de la variable
+            // Añadir ':' después de cada 'case' y corregir la sintaxis del 'switch'
+            switch ($tipoSubida) {
+
+                case "Enunciado":
+                    $destino = "../public/assets/media/enunciados/";
+                    break;
+
+                case "Solucion":
+                    $destino = "../public/assets/media/soluciones/";
+                    break;
+
+                default:
+                    // Si el tipo no coincide con los valores permitidos, puedes redirigir o manejar el error aquí
+                    die("Tipo de subida no permitido.");
+
+            }
+
+            // Verificamos si se ha definido una ruta de destino
+            if (!empty($destino)) {
+
+                $nombres = $_FILES['Ficheros']['name'];
+                $nombresTemporales = $_FILES['Ficheros']['tmp_name'];
+
+                for ($x = 0; $x < count($nombresTemporales); $x++) {
+
+                    if (is_file($nombresTemporales[$x])) {
+
+                        // Construimos el nombre completo para el archivo de destino
+                        $nombreCompleto = $destino.$nombres[$x];  // basename para evitar rutas problemáticas
+                        move_uploaded_file($nombresTemporales[$x], $nombreCompleto);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+        exit;
+        
+    } else if($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Obtiene el contenido JSON del cuerpo de la solicitud
         $json = file_get_contents('php://input');
         // Decodifica el JSON en un array asociativo

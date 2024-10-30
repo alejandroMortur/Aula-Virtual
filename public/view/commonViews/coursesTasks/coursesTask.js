d49e4738b2ec8372ef04ffc972626cbfd61ef614
AlexDEV <moronturielalejandro@gmail.com>
2024-10-30 23:19:22 +0100

    courseTask sidebar css js and html seted for view data and sort task

--- public/view/commonViews/coursesTasks/coursesTask.js
+++ public/view/commonViews/coursesTasks/coursesTask.js
@@ -1,9 +1,13 @@
 let userCoursesTaskList = []
+let currentCourseSorted;
 
 function printTask(currentCourse){
 
     let taskContainer = document.getElementById('taskContainer');
+    taskContainer.innerHTML  = "";
     let tasks = currentCourse.getTask();
+
+    //mirar error metodos en objetos task cuando se sortea
 
     tasks.forEach(element => {
         
@@ -20,11 +24,11 @@ function printTask(currentCourse){
 
         let taskName = document.createElement('p')
         taskName.id ="task__p"
-        taskName.innerHTML = element.getName();
+        taskName.innerHTML = element.name;
         taskCard.appendChild(taskImage);
 
         let taskDescription = document.createElement('p')
-        taskDescription.innerHTML = element.getDescription();
+        taskDescription.innerHTML = element.description;
         taskCard.appendChild(taskDescription);
 
     });
@@ -51,9 +55,50 @@ function clickCard(event){
 
 }
 
+function orderTasks(selectedValue, courseTask) {
+
+    if (selectedValue === "Ascendente") {
+        courseTask.sort((a, b) => {
+            if(a.name>b.name)return 1
+            return -1
+        });
+    } else if (selectedValue === "Descendente") {
+        courseTask.sort((a, b) => {
+            if(b.name>a.name)return 1
+            return -1
+        });
+    }
+
+    currentCourseSorted.coursesTask.splice(0,currentCourseSorted.coursesTask.length);
+    courseTask.forEach(task => {
+        currentCourseSorted.setTask(task)
+    });
+
+    printTask(currentCourseSorted);
+
+}
+
 function newTask(){
 
     sessionStorage.setItem("newTask", JSON.stringify("new"));
     window.location.href = "/public/view/commonViews/tasks/task.html";
+
+}
+
+function buildSideBar(currentCourse,cuorseTask){
+
+    currentCourseSorted = currentCourse;
+
+    document.getElementById('bar__title').innerHTML =  currentCourse.title;
+    document.getElementById('bar__p').innerHTML =  currentCourse.description;
+    document.getElementById('bar__img').addEventListener('click', ()  =>  newTask());
+    
+    const selectElement = document.getElementById('realignment');
+
+    // Agregar el evento change al elemento select
+    selectElement.addEventListener('change', () => {
+        const selectedValue = selectElement.value; // Obtener el valor seleccionado
+        orderTasks(selectedValue,cuorseTask); // Llamar a la función con el valor seleccionado
+    });
 
 }

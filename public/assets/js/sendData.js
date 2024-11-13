1e3e291f4e9d20813a2d5b324706ccbf17be41ce
AlexDEV <moronturielalejandro@gmail.com>
2024-11-13 20:52:39 +0100

    sendData error and succes console.log set

--- public/assets/js/sendData.js
+++ public/assets/js/sendData.js
@@ -1,18 +1,22 @@
-function mandarDatos(objeto){
-
-    fetch('http://localhost:8080/server/main.php', {
+function mandarDatos(objeto) {
+    return fetch('http://localhost:8080/server/main.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(objeto)
     })
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error in the request');
+        }
+        return response.text(); 
+    })
     .then(data => {
-        console.log(data);
+        console.log('Data sent successfully:', data);
     })
     .catch(error => {
         console.error('Error:', error);
+        throw error; 
     });
-
 }
